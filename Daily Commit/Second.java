43b16ca56c9bf2900cd67a0e14f7b45985eb26c2
Akshatamitvyas <47518001+Akshatamitvyas@users.noreply.github.com>
2021-05-27 14:38:02 +0530

    Second Commit

---
+++ Daily Commit/Second.java
@@ -0,0 +1,9 @@
+// WAP which take Input from user and print the output.
+
+import java.util.Scanner;
+class Second{
+  public static void main(String args[]){
+    Scanner x = new Scanner(System.in);
+    System.out.println(x.nextLine());
+  }
+}
