ea6b1b273767b323e70c08f9186f013eac8e39a4
Akshatamitvyas <47518001+Akshatamitvyas@users.noreply.github.com>
2021-05-27 14:38:46 +0530

    Second Program

--- Daily Commit/Second.java
+++ Daily Commit/Second.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 class Second{
-  public static void main(String args[]){
+  public static void main(String args[]) {
     Scanner x = new Scanner(System.in);
     System.out.println(x.nextLine());
   }
